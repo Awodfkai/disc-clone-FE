aac369de012ca71947c083b654ca0a07e874b488
Brian Wang <brbwang@yahoo.com>
2020-11-02 03:45:34 -0500

    production

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:8000/api';
-export const noApiUrl = 'http://localhost:8000';
+export const baseUrl = `${process.env.REACT_APP_BASE_URL}/api` || 'http://localhost:8000/api';
+export const noApiUrl = process.env.REACT_APP_BASE_URL || 'http://localhost:8000';
